e231dddad9d1beb4ff1bb9c7d5a14c25a8ba0c95
Michal Szyndel <michal.szyndel@me.com>
2012-12-04 10:56:54 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedclothBug::Application.config.secret_token = 'a924396f3e56475d05579ae04ec50330742cb666d3414f0c2c09f7469c074795ccbf4ad030d4d6ebe5580edc92ed63910012b6df6412eb9d440d8edc9144bedd'
